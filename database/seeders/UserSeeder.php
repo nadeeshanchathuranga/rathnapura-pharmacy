172f9615a8861a8b95dba84a744249e6f8c0211c
N.D.V.Ovinya Perera <windypereira39@gmail.com>
2026-04-20 15:03:07 +0530

    Hash passwords for users in UserSeeder for improved security

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -16,46 +17,46 @@ class UserSeeder extends Seeder
         // Admin User - Full Access
         User::firstOrCreate(
             ['email' => 'admin@gmail.com'],
-            ['name' => 'Admin', 'password' => 'TRSRIYAS8696', 'role' => 0]
+            ['name' => 'Admin', 'password' => Hash::make('TRSRIYAS8696'), 'role' => 0]
         );
 
         // Backoffice - Can manage products and GRNs
         User::firstOrCreate(
             ['email' => 'backoffice@gmail.com'],
-            ['name' => 'Backoffice', 'password' => 'RR202020', 'role' => 1]
+            ['name' => 'Backoffice', 'password' => Hash::make('RR202020'), 'role' => 1]
         );
 
         // Cashier - access to all divisions
         User::firstOrCreate(
             ['email' => 'cashier1@gmail.com'],
-            ['name' => 'Cashier', 'password' => 'RR576700', 'role' => 2, 'division_id' => null]
+            ['name' => 'Cashier', 'password' => Hash::make('RR576700'), 'role' => 2, 'division_id' => null]
         );
 
         // Cashier - access to all divisions
         User::firstOrCreate(
             ['email' => 'cashier2@gmail.com'],
-            ['name' => 'Cashier', 'password' => 'CAPLUS12000', 'role' => 2, 'division_id' => null]
+            ['name' => 'Cashier', 'password' => Hash::make('CAPLUS12000'), 'role' => 2, 'division_id' => null]
         );
 
         // Token Counter Cashiers
         User::firstOrCreate(
             ['email' => 'tc1@gmail.com'],
-            ['name' => 'Token Cashier', 'password' => 'TRS1900', 'role' => 3, 'division_id' => null]
+            ['name' => 'Token Cashier', 'password' => Hash::make('TRS1900'), 'role' => 3, 'division_id' => null]
         );
 
         User::firstOrCreate(
             ['email' => 'tc2@gmail.com'],
-            ['name' => 'Token Cashier', 'password' => 'TRS3070', 'role' => 3, 'division_id' => null]
+            ['name' => 'Token Cashier', 'password' => Hash::make('TRS3070'), 'role' => 3, 'division_id' => null]
         );
 
         User::firstOrCreate(
             ['email' => 'tc3@gmail.com'],
-            ['name' => 'Token Cashier', 'password' => 'Thikshani7575', 'role' => 3, 'division_id' => null]
+            ['name' => 'Token Cashier', 'password' => Hash::make('Thikshani7575'), 'role' => 3, 'division_id' => null]
         );
 
         User::firstOrCreate(
             ['email' => 'tc4@gmail.com'],
-            ['name' => 'Token Cashier', 'password' => 'RIYAS156', 'role' => 3, 'division_id' => null]
+            ['name' => 'Token Cashier', 'password' => Hash::make('RIYAS156'), 'role' => 3, 'division_id' => null]
         );
 
     }
